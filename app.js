876f5b98b7053c1d418eca697da4f4f1049711d4
Peter Wildfeuer <wildfeuer.peter@gmail.com>
2024-02-27 20:06:26 -0500

    created load file button

--- app.js
+++ app.js
@@ -1,24 +1,3 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+let app = function(filepath){
 
-const PORT = process.env.PORT || 3000;
-
-const server = http.createServer((req, res) => {
-    const filePath = path.join(__dirname, 'index.html');
-
-    fs.readFile(filePath, (err, data) => {
-        if (err) {
-            res.writeHead(404, { 'Content-Type': 'text/html' });
-            return res.end('404 Not Found');
-        }
-
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        return res.end();
-    });
-});
-
-server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+}
